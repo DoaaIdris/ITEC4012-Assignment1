1ff581bae93d8f184e931c63b4cd8aaae8586bb6
Doaa <doaa.id00@gmail.com>
2021-09-30 22:46:24 -0400

    first API route added

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.send("Hi from the express server!");
 });
 
+app.get('/api/countries', (re, res) =>{
+    //return dummy data
+    const countries = ["Canada", "United States", "Russia", "China", "Japan", "Egypt", "Germany", "South Africa", "Australia", "UAE"];
+
+    //sending a response should be in JSON
+    res.send(countries);
+});
+
 
 // running the express http server on the port we specified
 app.listen(port, () =>{
